c1f9c47913dc12924ceda5a90469ef96890a2a1e
JK-junkin <kinoshitaj@affrc.go.jp>
2020-04-13 17:11:06 +0900

    :tada: Add test directory

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(lgbktestr)
+
+test_check("lgbktestr")
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,11 @@
+library(lgbktestr)
+context("Column names")
+
+test_that("uniform_df handles data frame", {
+  expect_warning(
+    df1 <- data.frame(A = logical(10L), B = integer(10L)) %>%
+      uniform_df(),
+    'Removed column\\(s\\): "A", "B"'
+  )
+  expect_equal(colnames(df1), create_ideal_colnames(fishery = "purse seine"))
+})
