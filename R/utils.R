b530c82720657271adbef6f4186f5df190153b64
JK-junkin <kinoshitaj@affrc.go.jp>
2020-04-13 17:14:01 +0900

    :recycle: devide uniform_df function

--- R/utils.R
+++ R/utils.R
@@ -12,9 +12,33 @@ str_rm_newline_code <- function(vector) {
     stringr::str_remove_all("\\n")
 }
 
+#' Genarater of ideal column names
+#'
+#' @param fishery fishery type of logbook
+#' @examples
+#' \dontrun{
+#' create_ideal_colnames(fishery = "purse seine")
+#' }
+create_ideal_colnames <- function(fishery) {
+  if (fishery == "purse seine") {
+    c("整理番号", "操業次", "報告月", "漁業種類コード",
+      "漁法コード", "県コード", "操業海域", "漁船一連番号",
+      "船名", "漁船登録番号", "トン数", "トン数別階層コード",
+      "航海数", "航海日数", "操業日数", "探索日数",
+      "通常従業員数", "漁協コード", "許可種類", "根拠地", "メモ",
+      "操業年月日", "操業月", "操業日", "大海区", "漁区",
+      "まいわし(小中)", "まいわし(大)", "かたくち(小中)",
+      "かたくち(大)", "うるめ(小中)", "うるめ(大)", "さば(小中)",
+      "さば(大)", "まあじ(小中)", "まあじ(大)", "まるあじ",
+      "むろあじ", "ぶり", "するめいか", "その他", "合計",
+      "操業メモ(毎日の操業の備考欄の事項")
+  }
+}
+
 #' Uniform (Standardize) data frame.
 #'
 #' @param df input data frame to be processed
+#' @param ideal_columns
 #' @return A data.frame ncol reduced if excess columns exists.
 #' @examples
 #' df <- data.frame(整理番号 = 1:3,
@@ -23,22 +47,12 @@ str_rm_newline_code <- function(vector) {
 #' uniform_df(df)
 #'  Warnings:
 #'   Removed column(s): '報告年'
-uniform_df <- function(df) {
-  ideal_columns <- c("整理番号", "操業次", "報告月", "漁業種類コード",
-                     "漁法コード", "県コード", "操業海域", "漁船一連番号",
-                     "船名", "漁船登録番号", "トン数", "トン数別階層コード",
-                     "航海数", "航海日数", "操業日数", "探索日数",
-                     "通常従業員数", "漁協コード", "許可種類", "根拠地", "メモ",
-                     "操業年月日", "操業月", "操業日", "大海区", "漁区",
-                     "まいわし(小中)", "まいわし(大)", "かたくち(小中)",
-                     "かたくち(大)", "うるめ(小中)", "うるめ(大)", "さば(小中)",
-                     "さば(大)", "まあじ(小中)", "まあじ(大)", "まるあじ",
-                     "むろあじ", "ぶり", "するめいか", "その他", "合計",
-                     "操業メモ(毎日の操業の備考欄の事項") %>%
+uniform_df <- function(df, fishery = "purse seine") {
+  ideal_columns <- create_ideal_colnames(fishery = fishery) %>%
     purrr::map_dfr(~ tibble::tibble(!!.x := logical()))
 
   processed <- df %>%
-    # magrittr::set_colnames(stringr::str_to_title(colnames(df))) %>%
+    magrittr::set_colnames(str_rm_newline_code(colnames(df))) %>%
     dplyr::bind_rows(ideal_columns)
 
   unknown_column <-
@@ -48,7 +62,9 @@ uniform_df <- function(df) {
 
   if (do_exist_unknown_column)
     warning(
-      paste0("Removed column(s): '", paste(unknown_column, collapse = ", '"))
+      paste0('Removed column(s): "',
+             paste0(unknown_column, collapse = '", "'),
+             '"')
       )
 
   processed %>%
