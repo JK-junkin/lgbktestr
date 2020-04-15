50a682edffce6ef8e6363cf7715ff6ec34b1d876
JK-junkin <50478487+JK-junkin@users.noreply.github.com>
2020-04-15 18:24:14 +0900

    Create tracer bullet product (#7)
    * :pencil: Change package name
    
    * :tada: Initial add export functions
    
    * :tada::camera_flash: Initial add try-error script
    
    * :tada: Initial add functions
    
    * :tada: Add test directory
    
    * :heavy_plus_sign: Add testthat in Suggests
    
    * :recycle: devide uniform_df function
    
    * :camera_flash: stash some modifications
    
    * :down: postpone stdout of warning
    
    * :up: small modification of function name
    
    * :see_no_evil: Add some files
    
    * :page_facing_up: sort and arrange packages
    
    * :cut_of_meat: Separate and Add new files
    
    * :tada::camera_flash: Add new and brush up start-up files
    
    * :pencil::tada::truck::recycle: create documents
    
    * :name_badge: Update NAMESPACE
    
    * :recycle: Update function names and their document
    
    * :tada::fire: Modify function documents
    
    * :arrow_up: Add a package in Remotes section
    
    * :up: Add some codes
    
    * :sparkles: Add hard code error checks
    
    * :recycle: Update data wrangling step
    
    * :poop: Add tracer code demonstrating to PO
    
    * :bug: Cope with check warnings
    
    * :bug: Cope with check warnings
    
    * :bulb: Update function documentations
    
    * :rotating_light: Remove linter warnings

---
+++ R/ui-inner.R
@@ -0,0 +1,115 @@
+#' Test length to be correctyly digitalized
+#'
+#' @param dat data frame including column to be scaned
+#' @param column a column name to be scaned
+#' @param u_len an unique value (ideal length) to be matched against
+#'
+#' @return any warning messages
+#'
+#' @importFrom testthat test_that expect_identical
+#' @importFrom magrittr %>%
+#' @importFrom stats na.omit
+#'
+#' @examples
+#' \dontrun{
+#' test_length_identical(dat, column = "foo", u_len = 1L)
+#' }
+test_length_identical <- function(dat, column, u_len) {
+  target <- dat[, column] %>%
+    unique() %>%
+    na.omit() %>%
+    length()
+
+  test_that("is operation region mixed?", {
+    expect_identical(target, u_len)
+  })
+}
+
+#' Test a number to be correctly digitalized
+#'
+#' @param dat data frame including column to be scaned
+#' @param column a column name to be scaned
+#' @param u_value an unique value (ideal number) to be matched against
+#'
+#' @return any warning messages
+#'
+#' @importFrom testthat test_that expect_identical
+#' @importFrom magrittr %>%
+#' @importFrom stats na.omit
+#'
+#' @examples
+#' \dontrun{test_year(dat, column = "foo", u_value = 2020)}
+test_value_identical <- function(dat, column, u_value) {
+  # target <- dat %>%
+  #   dplyr::filter(!is.na(column)) %>%
+  #   dplyr::pull(column) %>%
+  target <- dat[, column] %>%
+    unique() %>%
+    na.omit() %>%
+    as.integer()
+
+  test_that("is a value equal to?", {
+    expect_identical(target, u_value)
+  })
+}
+
+#' Test numbers to be correctly digitalized
+#'
+#' @param dat data frame including column to be inspected
+#' @param column a column name to be scaned
+#' @param u_values unique values (ideal numbers) to be matched against
+#'
+#' @return an warning message
+#'
+#' @importFrom testthat test_that expect_true
+#' @importFrom magrittr %>%
+#' @importFrom stats na.omit
+#'
+#' @examples
+#' \dontrun{
+#' test_region_numcode(dat, column = "foo", ideal_code_numbers = 1:2)
+#' }
+test_value_equals <- function(dat, column, u_values) {
+  # target <- dat %>%
+  #   dplyr::filter(!is.na(column)) %>%
+  #   dplyr::pull(column) %>%
+  target <- dat[, column] %>%
+    unique() %>%
+    na.omit() %>%
+    as.integer()
+
+  test_that("is code number correct?", {
+    expect_true(target %all_in% u_values)
+  })
+}
+
+#' Test total value corresponded with the sum of partial values
+#'
+#' @param dat data frame including column to be inspected
+#' @param total_col a column name of total value digitized
+#' @param partial_cols a list of column names to sum up
+#'
+#' @return an warning message
+#'
+#' @importFrom testthat test_that expect_equal
+#' @importFrom magrittr %>%
+#' @importFrom stats na.omit
+#'
+#' @examples
+#' \dontrun{
+#' test_sum(dat, total_col = "foobar", partial_cols = c("foo", "bar"))
+#' }
+test_sum <- function(dat, total_col, partial_cols = c()) {
+  target <- dat[, total_col] %>%
+    na.omit()
+
+  against <- dat %>%
+    # dplyr::select(`まいわし(小中)`:`その他`) %>%
+    dplyr::select_at(.vars = dplyr::vars(partial_cols)) %>%
+    rowSums(na.rm = F) %>%
+    na.omit()
+
+  test_that("is total value equal to each partial values?", {
+    expect_equal(target, against)
+  })
+}
