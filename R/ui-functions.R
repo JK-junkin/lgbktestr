169bd661777531a2c9fba8869b6b29feed93f260
JK-junkin <kinoshitaj@affrc.go.jp>
2020-05-16 14:26:56 +0900

    :art: Modify body and formals of ui-functions

--- R/ui-functions.R
+++ R/ui-functions.R
@@ -10,7 +10,9 @@
 #' }
 #' @export
 check_logbook <- function(file, ...) {
-  class(file) <- append(tools::file_ext(file), class(file))
+  ext <- tools::file_ext(file)
+  if (ext %in% c("xls", "xlsx")) ext <- "excel"
+  class(file) <- append(ext, class(file))
   UseMethod("check_logbook", file)
 }
 
@@ -23,19 +25,52 @@ check_logbook.default <- function(file, ...) {
   rlang::abort(warn)
 }
 
+#' Check up logbook excel file.
+#'
+#' @param file an excel file
+#' @param sheet an input sheet in the input file
+#' @param fishery fishery which the logbook treats
+#' @param ... arguments succeeded to each method function
+#' @return data.frame describing error or warning records
+#'
+#' @examples
+#' \dontrun{
+#' treat_excel(file = "your/file.xlsx",
+#'             sheet = "Sheet 1",
+#'             fishery = "purse_seine"))
+#' }
 #' @export
-check_logbook.xls <- function(file, ...) {
-  treat_excel(file, ...)
-}
-
-#' @export
-check_logbook.xlsx <- function(file, ...) {
-  treat_excel(file, ...)
+check_logbook.excel <- function(file, sheet, fishery, ...) {
+  sheets <- readxl::excel_sheets(path = file)
+  
+  if (missing(sheet)) {
+    sheet <- 1
+    sheet_name <- sheets[sheet]
+    message("'sheet' is missing then 1st sheet '", sheet_name,
+            "' is read by default." )
+  } else if (is.numeric(sheet)) {
+    sheet_name <- sheets[sheet]
+  } else {
+    sheet_name <- sheet
+  }
+  
+  # This code is a hard coding for the business needs.
+  if (missing(fishery)) {
+    message("'fishery' is missing then 'purse_seine' is assigned automatically.")
+    fishery <- "purse_seine"
+  }
+  
+  dat <- readxl::read_excel(path = file, sheet = sheet_name) %>%
+    uniform_df(.fishery = fishery)
+  class(dat) <- append(fishery, class(dat))
+  
+  scan_contents(.dat = dat, ...,
+                .file = file, .sheet = sheet_name, .fishery = fishery)
 }
 
 #' @export
 check_logbook.csv <- function(file, ...) {
-  treat_csv(file, ...)
+  dat <- read.csv(file, stringsAsFactors = FALSE)
 }
 
 
