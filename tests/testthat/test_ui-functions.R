a60f7e25964d0fabdca71889efd360ded9becab8
JK-junkin <kinoshitaj@affrc.go.jp>
2020-05-16 17:40:24 +0900

    :art: Combine treat_xxx tests

--- tests/testthat/test_ui-functions.R
+++ tests/testthat/test_ui-functions.R
@@ -1,11 +1,19 @@
 library(lgbktestr)
 context("I/O: check_logbook()")
 
-test_that("check_logbook() returns data.frame", {
+test_that("check_logbook.excel() returns data.frame", {
   expect_warning(
     expect_is(
       check_logbook("excel/PS-dummy-1.xlsx"), "data.frame"
       ),
     'Removed column\\(s\\): "...44", "...45"'
+  )
+})
+
+test_that("check_logbook.csv() returns data.frame", {
+  df <- data.frame(a = letters)
+  expect_message(
+    check_logbook.csv(df),
+    "CSV file can't be treated for now. Please wait until release."
   )
 })
